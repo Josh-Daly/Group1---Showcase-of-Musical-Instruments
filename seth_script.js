e1e716749560fdc740d9ba3f10438addb929e076
SethATU <g00467621@atu.ie>
2024-12-10 22:57:58 +0000

    Adding a audio button

---
+++ seth_script.js
@@ -0,0 +1,29 @@
+function playDrumsAudio() 
+{
+    var x = document.getElementById("audio");
+    x.play(); 
+}
+
+function playStickAudio() 
+{
+    var x = document.getElementById("audio");
+    x.play(); 
+}  
+
+function playHeadAudio() 
+{
+    var x = document.getElementById("audio");
+    x.play(); 
+}  
+
+function playCymbolAudio() 
+{
+    var x = document.getElementById("audio");
+    x.play(); 
+}  
+
+function playHardwarAudio() 
+{
+    var x = document.getElementById("audio");
+    x.play(); 
+}  
