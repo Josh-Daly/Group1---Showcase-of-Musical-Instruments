b3e6e0d7185480f1efa421fa1fbd0d4807f5d79e
Josh <g00470372@atu.ie>
2024-12-11 12:33:48 +0000

    Added pics and comment section

--- paul_index.js
+++ paul_index.js
@@ -5,8 +5,11 @@ function makeComment()
     var name = document.getElementById("name").value;
     var country = document.getElementById("country").value;
     var instrument = document.getElementById("instrument").value;
-    document.getElementById("commentUpdate").innerHTML= "<p>" + name + " " + country + " " + instrument + "</p>";
+    var text = document.getElementById("commentText").value;
 
-
+    document.getElementById("commText").value = text;
+    document.getElementById("commName").innerHTML = "<p> Name:" + " " + name + "</p>";
+    document.getElementById("commCoun").innerHTML = "<p> Country:" + " " + country + "</p>";
+    document.getElementById("commInst").innerHTML = "<p> Favorite Instument:" + " " + instrument + "</p>";
     document.getElementById("commentUpdate").style.display = "block";
 }
