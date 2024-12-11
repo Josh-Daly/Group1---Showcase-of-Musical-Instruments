3edb0864b0f4b6fa76f0b66580187bcbe9f5957d
Josh <g00470372@atu.ie>
2024-12-11 10:58:25 +0000

    Working on javascript stuff

--- paul_index.js
+++ paul_index.js
@@ -1,4 +1,12 @@
+document.getElementById("commentUpdate").style.display = "none";
+
 function makeComment()
 {
-    document.getElementById(comment).value= document.getElementById(commentTest).value;
+    var name = document.getElementById("name").value;
+    var country = document.getElementById("country").value;
+    var instrument = document.getElementById("instrument").value;
+    document.getElementById("commentUpdate").innerHTML= "<p>" + name + " " + country + " " + instrument + "</p>";
+
+
+    document.getElementById("commentUpdate").style.display = "block";
 }
