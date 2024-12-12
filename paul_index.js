a313a1a67345a46acf9b8de01ef89a0319386b93
Josh <g00470372@atu.ie>
2024-12-12 12:25:44 +0000

    Added most content, added a suggestion box

--- paul_index.js
+++ paul_index.js
@@ -1,4 +1,5 @@
 document.getElementById("commentUpdate").style.display = "none";
+document.getElementById("suggested").style.display = "none";
 
 function makeComment()
 {
@@ -8,8 +9,17 @@ function makeComment()
     var text = document.getElementById("commentText").value;
 
     document.getElementById("commText").value = text;
+    document.getElementById("commText").style = "resize:none;background-color:#DCDCDC;"
     document.getElementById("commName").innerHTML = "<p> Name:" + " " + name + "</p>";
     document.getElementById("commCoun").innerHTML = "<p> Country:" + " " + country + "</p>";
     document.getElementById("commInst").innerHTML = "<p> Favorite Instument:" + " " + instrument + "</p>";
     document.getElementById("commentUpdate").style.display = "block";
+}
+
+function makeSuggestion()
+{
+    var suggest = document.getElementById("suggestText").value;
+    document.getElementById("suggested").style.display = "block";
+    document.getElementById("suggested").innerHTML = "<p> Thank you for your suggestion: <mark>" + suggest + "</mark> </p>";
+    document.getElementById("suggestion").style.display = "none";
 }
